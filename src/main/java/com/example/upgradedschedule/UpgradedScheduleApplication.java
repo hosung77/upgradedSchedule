947901d6c12516c930e11538a7479a706ba1153f
hosung77 <ho4728@gmail.com>
2025-04-02 18:05:35 +0900

    setting

--- src/main/java/com/example/upgradedschedule/UpgradedScheduleApplication.java
+++ src/main/java/com/example/upgradedschedule/UpgradedScheduleApplication.java
@@ -2,7 +2,9 @@ package com.example.upgradedschedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class UpgradedScheduleApplication {
 
