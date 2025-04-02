dc5bc89a2ebbae6bf0953c4a8997323e4a79a19f
hosung77 <ho4728@gmail.com>
2025-04-02 18:00:10 +0900

    first commit

---
+++ src/main/java/com/example/upgradedschedule/UpgradedScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.upgradedschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradedScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpgradedScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/upgradedschedule/UpgradedScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upgradedschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpgradedScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
